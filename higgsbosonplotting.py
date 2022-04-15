9cf9c03da6a58bae78f1c9c1db719963a5b73469
jestokes22 <52247432+jestokes22@users.noreply.github.com>
2022-04-15 17:37:33 -0400

    Update higgsbosonplotting.py

--- higgsbosonplotting.py
+++ higgsbosonplotting.py
@@ -19,3 +19,28 @@ plt.ylabel('Amount')
 plt.title('Histogram of the invariant masses')
 
 plt.show()
+
+# Bump around 90GeV's
+min = 85
+max = 97
+
+# Let's crop the area. croMass now includes all the masses between the values of min and max
+croMass = iMass[(min < iMass) & (iMass < max)]
+
+# Calculate the mean (µ) and standard deviation (sigma) of normal distribution using norm.fit-function from scipy
+(mu, sigma) = norm.fit(croMass)
+
+# Histogram of the cropped data. Note that the data is normalized (density = 1)
+n, bins, patches = plt.hist(croMass, 300, density = 1, facecolor='g')
+
+#mlab.normpdf calculates the normal distribution's y-value with given µ and sigma
+# let's also draw the distribution to the same image with histogram
+y = norm.pdf(bins, mu, sigma)
+l = plt.plot(bins, y, 'r-.', linewidth=3)
+
+
+plt.xlabel('Invarian Mass(GeV)')
+plt.ylabel('Probability')
+plt.title(r'$\mathrm{Histogram \ and\ fit,\ where:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
+
+plt.show()
